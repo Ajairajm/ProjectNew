77c9c00cf69ebaac506fdeac7ff6a6ed8e9a67df
Ajairajm <m.r.ajairaj@gmail.com>
2021-12-29 10:41:17 +0530

    Task one

---
+++ src/org/emp/Company.java
@@ -0,0 +1,29 @@
+package org.emp;
+
+public class Company extends Employee {
+
+	
+	 public Company() {
+		 super(307.777f);
+		 System.out.println("child default constructor");
+	 
+	 }
+	 	public Company (String name) {
+	 		this ("chennai",3453625);
+	 		System.out.println("String para conc."+name);
+	 		
+	 	}
+	 	
+	 	public Company(String city, int id) {
+	 		System.out.println("string int para conc"+city+"/t"+id);
+	 		
+	 	}
+	 	public static void main(String[] args) {
+			Company c=new Company();
+			Company c1=new Company("ajai");
+			
+			
+		}
+
+
+}
---
+++ src/org/emp/Employee.java
@@ -0,0 +1,16 @@
+package org.emp;
+
+public class Employee {
+	
+	public Employee() {
+		
+		System.out.println("parent default constructor");
+		
+	}
+	
+	public Employee(float salary) {
+		System.out.println("one hour "+salary);
+		
+	}
+
+}
